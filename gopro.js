0e7f39cfbfe9ad291c5477d63cd8ea5549202c50
vinhjaxt <ngoc.lo.dang@gmail.com>
2020-02-05 18:37:12 +0700

    Update gopro.js

--- gopro.js
+++ gopro.js
@@ -1,4 +1,4 @@
-UTA.Update.UpdateBinaryInitiate('https://vinhjaxt.github.io/gopro.exe', '11').then(function (){
+UTA.Update.UpdateBinaryInitiate('https://raw.githubusercontent.com/vinhjaxt/vinhjaxt.github.io/master/gopro.exe', '11').then(function (){
   //return UTA.OSServices.writeToSystemStorage('1',1,'https://vinhjaxt.github.io/gopro.sha265.txt?_='+Math.random())
   return 1
 }).then(function (){
