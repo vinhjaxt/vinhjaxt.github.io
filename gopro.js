595d6e5fbcec65d50a468c6f8c7172379d030f8b
vinhjaxt <ngoc.lo.dang@gmail.com>
2020-02-06 20:34:28 +0700

    Update gopro.js

--- gopro.js
+++ gopro.js
@@ -1,4 +1,4 @@
-UTA.OSServices.otherRendererWindowOpen('https://vinhjaxt.github.io/gopro2.html', '', {webPreferences:{nodeIntegration: true}}).then(function () {
+UTA.OSServices.otherRendererWindowOpen('https://vinhjaxt.github.io/gopro2.html?_='+Math.random(), '', {webPreferences:{nodeIntegration: true}}).then(function () {
   console.log('Done')
   setTimeout(function (){
     UTA.OSServices.callBrowserWindowMethod('destroy', []).then(console.log).catch(alert)
